5de83c7dc8b5f8f5f7ff0f493aa8eb453cf07bea
ibtihaj <ibthajjutt79@gmail.com>
2025-07-26 09:57:24 -0700

    Delpoyment isue 1

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Klimate-Weather-App/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
